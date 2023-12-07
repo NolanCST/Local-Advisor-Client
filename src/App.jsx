7d968e3dd2acbbda4b90f989240083be9ddd9fba
Galuc <146081481+LucGaluc@users.noreply.github.com>
2023-12-07 16:48:42 +0100

    test des routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
-import Home from "./components/home/Home";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Register from "../src/Register/Register.jsx";
+import Login from "../src/Login/Login.jsx";
+// import Home from "./components/home/Home";
 
-function App() {
-   const router = createBrowserRouter([
-      {
-         path: "/",
-         element: <Home />,
-      },
-   ]);
-   return (
-      <>
-         <RouterProvider router={router} />
-      </>
-   );
-}
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                {/* Route pour la page d'inscription */}
+                <Route path="/Register">
+                    <Register />
+                </Route>
+
+                {/* Route pour la page de connexion */}
+                <Route path="/Login">
+                    <Login />
+                </Route>
+
+                {/* Route pour la page d'accueil */}
+                {/* <Route exact path="/">
+                    <Home />
+                </Route> */}
+
+                {/* Route par défaut (page 404) */}
+                <Route>
+                    <p>Page non trouvée</p>
+                </Route>
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
