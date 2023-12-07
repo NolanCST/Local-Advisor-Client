3b267187198613272543184ff75a1c603ee0943b
AndreaBGLR <andrea.baglieri@laposte.net>
2023-12-07 14:25:29 +0100

    crea home.jsx

---
+++ Components/home/home.jsx
@@ -0,0 +1,19 @@
+import "./home.css";
+
+function Home(props) {
+  // fonctions
+  return (
+    <div className="homeContainer">
+      <div>{/* Emplacement navbar */}</div>
+      <section>
+        <div>
+          <div>
+            <h1>LocalAdvisor</h1>
+          </div>
+        </div>
+      </section>
+      <footer>{/* emplacement footer */}</footer>
+    </div>
+  );
+}
+export default Home;
