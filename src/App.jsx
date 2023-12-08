3c3b4da4ae178e00fb5d657b976a249ad2f6cf31
NolanCST <nolan.costa06@gmail.com>
2023-12-08 14:35:53 +0100

    Mise au propre pour les dossiers et les routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Register from "./register/Register";
-import Login from "./login/Login";
-// import Home from "./components/home/Home";
+import Register from "./components/register/Register";
+import Login from "./components/login/Login";
+import Home from "./components/home/Home";
 
 function App() {
-    const router = createBrowserRouter([
-        {
-            path: "/register",
-            element: <Register />,
-        },
-        {
-            path: "/login",
-            element: <Login />,
-        },
-        {
-            path: "/",
-            element: <Home />,
-        },
-    ]);
-    return (
-        <>
-            <RouterProvider router={router} />
-        </>
-    );
+   const router = createBrowserRouter([
+      {
+         path: "/",
+         element: <Home />,
+      },
+      {
+         path: "/register",
+         element: <Register />,
+      },
+      {
+         path: "/login",
+         element: <Login />,
+      },
+   ]);
+   return (
+      <>
+         <RouterProvider router={router} />
+      </>
+   );
 }
 
 export default App;
