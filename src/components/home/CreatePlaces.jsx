4660f43299fcc0777b98028649a98468a2e9e2e3
NolanCST <nolan.costa06@gmail.com>
2023-12-12 22:39:38 +0100

    Ajout du choix des catégories pour les lieux

--- src/components/home/CreatePlaces.jsx
+++ src/components/home/CreatePlaces.jsx
@@ -1,86 +1,133 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./CreatePlaces.css";
 
 function CreatePlaces() {
-  const [formData, setFormData] = useState({
-    name: "",
-    address: "",
-    city: "",
-    zip_code: "",
-    categories: "",
-    description: "",
-    image: null,
-  });
+   const [formData, setFormData] = useState({
+      name: "",
+      address: "",
+      city: "",
+      zip_code: "",
+      description: "",
+      image: null,
+   });
+   const [categories, setCategories] = useState([]);
+   const [selectedCategories, setSelectedCategories] = useState([]);
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+   const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+   };
 
-  const handleFile = (e) => {
-    const file = e.target.files[0];
-    setFormData({ ...formData, image: file });
-  };
+   const handleFile = (e) => {
+      const file = e.target.files[0];
+      setFormData({ ...formData, image: file });
+   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const formDataToSend = new FormData();
-    formDataToSend.append("name", formData.name);
-    formDataToSend.append("address", formData.address);
-    formDataToSend.append("city", formData.city);
-    formDataToSend.append("zip_code", formData.zip_code);
-    formDataToSend.append("description", formData.description);
-    formDataToSend.append("user_id", 1);
-    // formDataToSend.append("image", formData.image);
-
-    try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/places`, {
-        method: "POST",
-        body: formDataToSend,
-      });
-
-      const responseData = await response.json();
-
-      if (response.ok) {
-        console.log("Création réussie:", responseData);
-      } else {
-        console.error(
-          "Erreur lors de la création:",
-          responseData || response.statusText
-        );
+   const handleCategoryChange = (e) => {
+      const selectedCategory = e.target.value;
+      if (!selectedCategories.includes(selectedCategory)) {
+         setSelectedCategories([...selectedCategories, selectedCategory]);
       }
-    } catch (error) {
-      console.error("Erreur lors de la création:", error);
-    }
-  };
+      console.log(selectedCategories);
+   };
 
-  return (
-    <div className="CreatePlacesContainer">
-      <div className="wrapper">
-        <form encType="multipart/form-data" onSubmit={handleSubmit}>
-          <label htmlFor="name">Titre de l'activité:</label>
-          <input type="text" name="name" onChange={handleChange} />
-          <label htmlFor="address">Adresse:</label>
-          <input type="text" name="address" onChange={handleChange} />
-          <label htmlFor="city">Ville:</label>
-          <input type="text" name="city" onChange={handleChange} />
-          <label htmlFor="zip_code">Code Postal:</label>
-          <input type="number" name="zip_code" onChange={handleChange} />
-          <label htmlFor="categories">Catégories:</label>
-          <select name="categories" onChange={handleChange}>
-            <option value="category1">Category 1</option>
-            <option value="category2">Category 2</option>
-            {/* Ajoutez plus d'options au besoin */}
-          </select>
-          <label htmlFor="description">Description:</label>
-          <textarea name="description" onChange={handleChange}></textarea>
-          <label htmlFor="image">Sélectionner une image:</label>
-          <input type="file" name="image" onChange={handleFile} />
-          <button type="submit">Enregistrer</button>
-        </form>
+   const handleRemoveCategory = (categoryToRemove) => {
+      const updatedCategories = selectedCategories.filter((category) => category !== categoryToRemove);
+      setSelectedCategories(updatedCategories);
+   };
+
+   const recupCategories = async () => {
+      try {
+         const response = await fetch(`${import.meta.env.VITE_API_URL}/places/create`);
+         const data = await response.json();
+         setCategories(data);
+      } catch (error) {
+         console.error("Erreur lors de la recuperation des categories", error);
+      }
+   };
+
+   useEffect(() => {
+      recupCategories();
+   }, []);
+
+   const renderCategories = () => {
+      console.log(categories);
+      return categories?.map((element, index) => {
+         return (
+            <option key={index} value={element.id}>
+               {element.name}
+            </option>
+         );
+      });
+   };
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      const formDataToSend = new FormData();
+      formDataToSend.append("name", formData.name);
+      formDataToSend.append("address", formData.address);
+      formDataToSend.append("city", formData.city);
+      formDataToSend.append("zip_code", formData.zip_code);
+      formDataToSend.append("description", formData.description);
+      formDataToSend.append("user_id", 1);
+      formDataToSend.append("categories", JSON.stringify(selectedCategories));
+      // formDataToSend.append("image", formData.image);
+
+      try {
+         const response = await fetch(`${import.meta.env.VITE_API_URL}/places`, {
+            method: "POST",
+            body: formDataToSend,
+         });
+
+         const responseData = await response.json();
+         if (response.ok) {
+            console.log("Création réussie:", responseData);
+         } else {
+            console.error("Erreur lors de la création:", responseData || response.statusText);
+         }
+      } catch (error) {
+         console.error("Erreur lors de la création:", error);
+      }
+   };
+
+   return (
+      <div className="CreatePlacesContainer">
+         <div className="wrapper">
+            <form encType="multipart/form-data" onSubmit={handleSubmit}>
+               <label htmlFor="name">Titre de l'activité:</label>
+               <input type="text" name="name" onChange={handleChange} />
+               <label htmlFor="address">Adresse:</label>
+               <input type="text" name="address" onChange={handleChange} />
+               <label htmlFor="city">Ville:</label>
+               <input type="text" name="city" onChange={handleChange} />
+               <label htmlFor="zip_code">Code Postal:</label>
+               <input type="number" name="zip_code" onChange={handleChange} />
+               <label htmlFor="categories">Catégories:</label>
+               <select name="categories" onChange={handleCategoryChange} multiple>
+                  {renderCategories()}
+               </select>
+
+               <div>
+                  <p>Catégories sélectionnées:</p>
+                  <ul>
+                     {selectedCategories.map((category) => (
+                        <li key={category}>
+                           {category}
+                           <button onClick={() => handleRemoveCategory(category)}>Retirer</button>
+                        </li>
+                     ))}
+                  </ul>
+               </div>
+
+               <label htmlFor="description">Description:</label>
+               <textarea name="description" onChange={handleChange}></textarea>
+               <label htmlFor="image">Sélectionner une image:</label>
+               <input type="file" name="image" onChange={handleFile} />
+               <button type="submit">Enregistrer</button>
+            </form>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default CreatePlaces;
