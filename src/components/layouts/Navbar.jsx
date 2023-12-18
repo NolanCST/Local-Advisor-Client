4cec250cc765a406861c9c8f36ef4dc0cd2a22f9
LADebey <louisadriendebey@gmail.com>
2023-12-18 09:28:52 +0100

    Navbar commit 4

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,55 +1,62 @@
 import "./Navbar.css";
 
 function Navbar() {
-   const token = localStorage.getItem("token");
-   const isLoggedIn = !!token;
+  const token = localStorage.getItem("token");
+  const isLoggedIn = !!token;
 
-   return (
-      <div>
-         <nav className="navTag">
-            <a className="imageLink" href="/">
-               <img className="logoSmall" src="src\components\layouts\Corgi_guge-removebg-preview.png" />
-            </a>
-            <div className="navbar">
-               <ul className="navbarlist">
-                  <li>
-                     <a className="link" href="/">
-                        Accueil
-                     </a>
-                  </li>
-                  <li>
-                     <a className="link" href="/create">
-                        Ajouter
-                     </a>
-                  </li>
-                  <li>
-                     <a className="link" href="/profile">
-                        Profil
-                     </a>
-                  </li>
-                  <li>
-                     {isLoggedIn ? (
-                        <a className="link" href="/logout">
-                           Déconnexion
-                        </a>
-                     ) : (
-                        <>
-                           <div>
-                              <a className="link" href="/login">
-                                 Connexion
-                              </a>
-                              <a className="link" href="/register">
-                                 Inscription
-                              </a>
-                           </div>
-                        </>
-                     )}
-                  </li>
-               </ul>
-            </div>
-         </nav>
-      </div>
-   );
+  return (
+    <div>
+      <nav className="navTag">
+        <a className="imageLink" href="/">
+          <img
+            className="logoSmall"
+            src="src\components\layouts\Corgi_guge-removebg-preview.png"
+          />
+        </a>
+        <div className="navbar">
+          <ul className="navbarListLeft">
+            <li>
+              <a className="link" href="/">
+                Accueil
+              </a>
+            </li>
+            <li>
+              <a className="link" href="/create">
+                Ajouter
+              </a>
+            </li>
+            <li>
+              <a className="link" href="/profile">
+                Profil
+              </a>
+            </li>
+          </ul>
+          <ul className="navbarListRight">
+            {isLoggedIn ? (
+              <li>
+                <a className="link" href="/logout">
+                  Déconnexion
+                </a>
+              </li>
+            ) : (
+              <>
+                <li>
+                  <a className="link" href="/register">
+                    Inscription
+                  </a>
+                </li>
+                <li>
+                  <a className="link" href="/login">
+                    Connexion
+                  </a>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
