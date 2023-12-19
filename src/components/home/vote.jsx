e25fb35b2cb440e2b283f8b2ef1b91bd590ad5fe
AndreaBGLR <andrea.baglieri@laposte.net>
2023-12-19 09:34:14 +0100

    sauvegarde up et down

--- src/components/home/vote.jsx
+++ src/components/home/vote.jsx
@@ -1,23 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Vote = () => {
   const [count, setCount] = useState(0);
   const [liked, setLiked] = useState(false);
 
-  const like = () => {
-    if (liked == true) {
-      setCount(newCount => newCount - 1);
-    } else {
-      setCount(newCount => newCount + 1);
+  useEffect(() => {
+    const savedCount = localStorage.getItem('voteCount');
+    const savedLiked = localStorage.getItem('voteLiked');
+
+    if (savedCount) {
+      setCount(Number(savedCount));
     }
-    setLiked(newLiked => !newLiked);
+
+    if (savedLiked) {
+      setLiked(savedLiked === 'true');
+    }
+  }, []);
+
+  const like = () => {
+    setCount((newCount) => {
+      const updatedCount = liked ? newCount - 1 : newCount + 1;
+      localStorage.setItem('voteCount', updatedCount);
+      localStorage.setItem('voteLiked', !liked);
+
+      return updatedCount;
+    });
+
+    setLiked((newLiked) => !newLiked);
   };
 
   return (
     <div>
-      <button onClick={like}>
-        {liked ? '👎' : '👍'}
-      </button>
+      <button onClick={like}>{liked ? '👎' : '👍'}</button>
       <span>{count}</span>
     </div>
   );
