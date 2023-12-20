b3e58b49a665a121d2e1fe850992ffc0ad7356c7
NolanCST <nolan.costa06@gmail.com>
2023-12-20 13:55:54 +0100

    reglage beug

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,116 +1,106 @@
-
 import { useStatus } from "../status/StatusContext";
 import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function Navbar() {
    const { status } = useStatus();
    const navigate = useNavigate();
-   const token = localStorage.getItem("token");
    const [isOpen, setIsOpen] = useState(false);
 
    const toggleMenu = () => {
       setIsOpen(!isOpen);
    };
-    async function handleLogout() {
-        const isConfirmed = window.confirm(
-            "Êtes-vous sûr de vouloir vous déconnecter ?"
-        );
+   async function handleLogout() {
+      const isConfirmed = window.confirm("Êtes-vous sûr de vouloir vous déconnecter ?");
 
-        if (isConfirmed) {
-            // Supprime les infos de connexion du localStorage
-            localStorage.removeItem("email");
-            localStorage.removeItem("password");
-            localStorage.removeItem("token");
+      if (isConfirmed) {
+         // Supprime les infos de connexion du localStorage
+         localStorage.removeItem("email");
+         localStorage.removeItem("password");
+         localStorage.removeItem("token");
 
-            try {
-                // Requête vers API pour révoquer le token
-                const response = await fetch(
-                    `${import.meta.env.VITE_API_URL}/logout`,
-                    {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                    }
-                );
+         try {
+            // Requête vers API pour révoquer le token
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/logout`, {
+               method: "POST",
+               headers: {
+                  "Content-Type": "application/json",
+               },
+            });
 
-                if (response.ok) {
-                    // Redirige l'utilisateur vers la page de connexion
-                    navigate("/Login");
-                }
-            } catch (error) {
-                console.error(
-                    "Erreur lors de la déconnexion côté serveur :",
-                    error
-                );
+            if (response.ok) {
+               // Redirige l'utilisateur vers la page de connexion
+               navigate("/Login");
             }
-        }
-    }
+         } catch (error) {
+            console.error("Erreur lors de la déconnexion côté serveur :", error);
+         }
+      }
+   }
 
-    return (
-    <div>
-        {/* Menu Hamburger pour les petits écrans */}
-      <div className="hamburgerMenu" onClick={toggleMenu}>
-        <div className={isOpen ? "line line1 active" : "line line1"}></div>
-        <div className={isOpen ? "line line2 active" : "line line2"}></div>
-        <div className={isOpen ? "line line3 active" : "line line3"}></div>
-      </div>
-      <nav className={`navTag ${isOpen ? "open" : ""}`}>
-        <div class="sidebar">
-          <a className="imageLink" href="/">
-            <img className="logoSmall" src="src\components\layouts\Corgi_guge-removebg-preview.png" />
-          </a>
-          <div className="navbar">
-            <ul className="navbarListLeft">
-                <li>
-                    <a className="link" href="/">
+   return (
+      <div>
+         {/* Menu Hamburger pour les petits écrans */}
+         <div className="hamburgerMenu" onClick={toggleMenu}>
+            <div className={isOpen ? "line line1 active" : "line line1"}></div>
+            <div className={isOpen ? "line line2 active" : "line line2"}></div>
+            <div className={isOpen ? "line line3 active" : "line line3"}></div>
+         </div>
+         <nav className={`navTag ${isOpen ? "open" : ""}`}>
+            {/* <div class="sidebar"> */}
+            <a className="imageLink" href="/">
+               <img className="logoSmall" src="src\components\layouts\Corgi_guge-removebg-preview.png" />
+            </a>
+            <div className="navbar">
+               <ul className="navbarListLeft">
+                  <li>
+                     <a className="link" href="/">
                         Accueil
-                    </a>
-                </li>
-                {(status === 0 || status === 1) && (
-                    <li>
+                     </a>
+                  </li>
+                  {(status === 0 || status === 1) && (
+                     <li>
                         <a className="link" href="/profile">
-                            Profil
+                           Profil
                         </a>
-                    </li>
-                )}
-                {status === 1 && (
-                    <li>
+                     </li>
+                  )}
+                  {status === 1 && (
+                     <li>
                         <a className="link" href="/create">
-                            Ajouter
+                           Ajouter
                         </a>
-                    </li>
-                )}
-            </ul>
-            <ul className="navbarListRight">
-                {status === 0 || status === 1 ? (
-                    <li>
+                     </li>
+                  )}
+               </ul>
+               <ul className="navbarListRight">
+                  {status === 0 || status === 1 ? (
+                     <li>
                         <a className="link" onClick={handleLogout}>
-                            Déconnexion
+                           Déconnexion
                         </a>
-                    </li>
-                ) : (
-                    <>
+                     </li>
+                  ) : (
+                     <>
                         <li>
-                            <a className="link" href="/register">
-                                Inscription
-                            </a>
+                           <a className="link" href="/register">
+                              Inscription
+                           </a>
                         </li>
                         <li>
-                            <a className="link" href="/login">
-                                Connexion
-                            </a>
+                           <a className="link" href="/login">
+                              Connexion
+                           </a>
                         </li>
-                    </>
-                )}
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
+                     </>
+                  )}
+               </ul>
+            </div>
+            {/* </div> */}
+         </nav>
+      </div>
+   );
 }
 
 export default Navbar;
